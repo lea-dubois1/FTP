2e5102e799e128a66bdc0e73d98dd4ce4067cb82
lea-dubois1 <lea.dubois@laplateforme.io>
2022-10-05 17:52:12 +0200

    Travail FTP

---
+++ Job7/reseau_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# installer proftpd avec tout ses modules, openssl et filezilla
+apt-get install proftpd-*
+apt-get install openssl
+apt-get install filezilla
+
+# Creer le dossier SSL et y generer la clef et le certificat
+mkdir /etc/proftpd/ssl
+openssl req -x509 -newkey rsa:2048 -keyout /etc/proftpd/ssl/proftpd.key.pem -out /etc/proftpd/ssl/proftpd.cert.pem -nodes -days 365
+
+
+# modifier les fichier de conf : proftpd.conf ; tls.conf et modules.conf
+cp modules.conf /etc/proftpd/modules.conf
+cp proftpd.conf /etc/proftpd/proftpd.conf
+cp tls.conf /etc/proftpd/tls.conf
+
+# On redémarre proftpd
+systemctl restart proftpd
+
+# creer les utilisateurs Merry et Pippin
+useradd -m -p kalimac merry
+useradd -m -p secondbreakfast pippin
