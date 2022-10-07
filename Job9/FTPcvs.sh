1141eedfbdc71cf276e41a28c41a88af9520797f
lea-dubois1 <lea.dubois@laplateforme.io>
2022-10-07 09:20:27 +0200

    ajout de ,,

--- Job9/FTPcvs.sh
+++ Job9/FTPcvs.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 my_input=Shell_Userlist.csv
-groupadd ftpgroup
+sudo groupadd ftpgroup
 sudo cp proftpd.conf /etc/proftpd/proftpd.conf
 
 while IFS="," read Id Prenom Nom Mdp Role;
 do 
       if [ "$Id" -eq "$Id" ] 2>/dev/null;
             then
-            Mdp=${Mdp:0:6}
             sudo useradd -u $Id -p $Mdp $Prenom  
-            sudo usermod -aG ftpgroup $Prenom    
+            sudo usermod -aG ftpgroup ${Prenom, ,}    
 
             if [ ${Role:0:5} = "Admin" ];
                   then
